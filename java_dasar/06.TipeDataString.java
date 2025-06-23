6dfe17bb273d0d9622efc0ed75bc495919bef872
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-06-23 17:03:21 +0700

    Create 06.TipeDataString.java

---
+++ java_dasar/06.TipeDataString.java
@@ -0,0 +1,15 @@
+public class TipeDataString {
+    public static void main(String[] args) {
+        String firstName = "hoshimi";
+        String lastName = "miyabi";
+
+        String fullname = firstName + " " + lastName;
+
+        System.out.println(fullname);
+    }
+}
+
+/*
+tulis String dengan huruf besar awalnya.
+
+*/
