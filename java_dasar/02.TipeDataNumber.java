0a2482996b25f49adb01d4daad200bb9647a0487
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-06-23 12:36:04 +0700

    Create 02.TipeDataNumber.java

---
+++ java_dasar/02.TipeDataNumber.java
@@ -0,0 +1,42 @@
+public class TipeDataNumber {
+    public static void main(String[] args) {
+
+        byte iniByte = 100;
+        short iniShort = 10000;
+        int iniInt = 100000000;
+        long iniLong = 1000000000;
+        long iniLong2 = 10000000000L;
+
+        float iniFloat = 10.10F;
+        double iniDouble = 10.10;
+
+        // literals
+        int decimalInt = 34;
+        int hexaDecimal = 0xFFFF;
+        int binaryDecimal = 0b1100101;
+
+        //underscore
+        int amount = 1_000_000_000;
+        int sum = 60_000_000;
+
+    }
+}
+
+
+/*
+integer number :
+tipe data = min - max, size, default
+
+byte = -128 -> 127, 1 bytes, 0
+short = -32.768 -> 32.767, 2 bytes, 0
+int = -2.147.483.648 -> 2.147.483.647, 4 byter, 0
+long = -9.223.372.036.854.775.808 -> 9.223.372.036.854.775.807, 8 bytes, 0
+
+floating point number :
+Tipe data = min -> max, size, default
+
+float = 3.4e-038 -> 3.4e+038, 4 bytes, 0.0
+double = 1.7e-308-> 1.7e+308, 8 bytes, 0.0
+
+
+ */
