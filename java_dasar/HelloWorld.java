c5a4308880385f51acec78bd5478a3f9b33427ba
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-06-23 11:06:44 +0700

    Create HelloWorld.java

---
+++ java_dasar/HelloWorld.java
@@ -0,0 +1,7 @@
+// HellowWorld adalah main classnya, namanya harus sama dengan filenya
+public class HelloWorld {
+    // ketik psvm untuk mempercepat
+    public static void main(String[] args) {
+        System.out.println("hello world"); // jangan lupa titik koma
+    }
+}
