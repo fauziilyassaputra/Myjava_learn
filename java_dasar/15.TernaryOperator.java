25d753bcfe7cbe765e5a28f0c955a96b4fbab59e
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-06-26 16:54:37 +0700

    Create 15.TernaryOperator.java

---
+++ java_dasar/15.TernaryOperator.java
@@ -0,0 +1,9 @@
+public class TernaryOpererator {
+
+    public static void main(String[] args) {
+
+        int nilai = 75;
+        String ucapan = nilai >= 75 ? "anda lulus" : "silahkan coba lagi";
+        System.out.println(ucapan);
+    }
+}
