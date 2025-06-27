41082788ec082f85356aaa7a8f2b9dffbd74403e
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-06-27 17:05:53 +0700

    Create 02.Object.java

---
+++ java_OOP/02.Object.java
@@ -0,0 +1,16 @@
+public class PersonApp {
+    public static void main(String[] args) {
+        var Person1 = new Person();
+        Person person2 = new Person();
+        Person person3;
+        person3 = new Person();
+    }
+}
+
+
+/*
+-object adalah data yang berisi field / properties  / attribute dan method /function /behavior
+-semua data bukan primitif dijava adalah object, dari mulai integer, boolean, character, string dan lainnya
+-object adalah hasil instansiasi dari sebuah class
+-untk membuat object kita bisa menggunakan kata kunci new, dan diikuti dengan nama class dan kurung()
+ */
