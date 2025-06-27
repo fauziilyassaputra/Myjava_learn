4530103eb89ca8c2f57a2d48a88e3cd449a4a4dd
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-06-27 20:15:13 +0700

    Create Person.java

---
+++ java_OOP/04.Method/Person.java
@@ -0,0 +1,15 @@
+public class Person {
+        //field
+        String name;
+        String address;
+        final String country = "indonesia";
+
+        //method
+        void sayHello(String paramName){
+                System.out.println("hello " + paramName + " my name is " + name);
+        }
+
+
+
+}
+
