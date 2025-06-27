991b7014f9f0bb3dcaf68cae8825bb97b9097b8a
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-06-27 09:23:00 +0700

    Create 19.Method.java

---
+++ java_dasar/19.Method.java
@@ -0,0 +1,19 @@
+public class Method {
+    public static void main(String[] args) {
+
+    sayHelloWorld();
+
+    }
+
+    static void sayHelloWorld(){
+        System.out.println("hello world 1");
+        System.out.println("hello world 2");
+        System.out.println("hello world 3");
+    }
+}
+
+/*
+- method adalah block kode program yang akan berjalan saat kita panggil
+- di bahasa pemograman lain, method juga disebut dengan function
+- kebiasan penulisan method diawali dengan huruf kecil baru besar untuk kata selanjutnya
+ */
