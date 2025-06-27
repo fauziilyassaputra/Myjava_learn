6a8340be9c1f2ee2a476361510ae28a236300911
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-06-27 16:55:13 +0700

    Create 01.Class.java

---
+++ java_OOP/01.Class.java
@@ -0,0 +1,11 @@
+public class Person {
+
+}
+
+/*
+- Class adalah blueprint untuk membuat object
+- Class berisikan deklarasi semua properties dan functions yang dimiliki oleh object
+- setiap object selalu dibuat dari class
+- sebuah class bisa membuat object tanpa batas
+
+ */
