43851c552a446b75739641fa4a316cdec758f117
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-06-28 17:54:13 +0700

    Create Product.java

---
+++ java_OOP/19.Import/data/Product.java
@@ -0,0 +1,17 @@
+package hollow.special.operation.data;
+
+public class Product {
+
+     public String name;
+     public int price;
+
+     public Product(String name, int price){
+         this.name = name;
+         this.price = price;
+     }
+
+}
+
+/*
+
+ */
