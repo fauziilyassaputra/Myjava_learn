7013b7e8b2b81bc3858175fe56fcfc47f58260c0
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-06-28 17:29:22 +0700

    Create Product.java

---
+++ java_OOP/16.AcsessModifier/Product.java
@@ -0,0 +1,31 @@
+package hollow.special.operation.data;
+
+// mana main class harus sama dengan nama filenya
+public class Product {
+
+//  jika fieldnya dalah private, maka tidak bisa dipanggil di file ProductApp
+
+//    private String name;
+//    private int price;
+
+    protected String name;
+    protected int price;
+
+     Product(String name, int price){
+         this.name = name;
+         this.price = price;
+     }
+
+}
+
+/*
+Access level :
+modifier        class       package         subclass        world
+
+public           Y             Y                Y             Y
+protected        Y             Y                Y             N
+no midifier      Y             Y                N             N
+private          Y             N                N             N
+
+
+ */
