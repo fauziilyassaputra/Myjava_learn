84e0e692295221ed1156006a023851a135c950a4
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-06-29 16:19:52 +0700

    Create ProductApp.java

---
+++ java_OOP/26.ToStringMethod/ProductApp.java
@@ -0,0 +1,14 @@
+package hollow.special.operation.data;
+
+public class ProductApp {
+    public static void main(String[] args) {
+
+        Product product = new Product("asus ROG zepherus", 50_000_000);
+        System.out.println(product.name);
+        System.out.println(product.price);
+
+        // panggil method toString yang telah dibuat
+        System.out.println(product);
+
+    }
+}
