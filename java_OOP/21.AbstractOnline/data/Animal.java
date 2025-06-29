4f744fe2a16f08510b5822bd467090bc267a0236
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-06-29 09:27:06 +0700

    Create Animal.java

---
+++ java_OOP/21.AbstractOnline/data/Animal.java
@@ -0,0 +1,11 @@
+package hollow.special.operation.data;
+
+public abstract class Animal {
+
+    public  String name;
+
+    // method tidak bisa di deklerasikan ({}) karena merupakan abstract
+    public abstract void run();
+}
+
+
