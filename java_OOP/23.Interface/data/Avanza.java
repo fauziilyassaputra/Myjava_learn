03ad27debbdecfe5e463656f4cd22ba18375efc7
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-06-29 13:55:38 +0700

    Create Avanza.java

---
+++ java_OOP/23.Interface/data/Avanza.java
@@ -0,0 +1,15 @@
+package hollow.special.operation.data;
+
+public class Avanza implements Car {
+
+    // method drive dan getTier wajib di implementasikan
+
+    public void drive() {
+        System.out.println("avanza drive");
+    }
+
+
+    public int getTier() {
+        return 4;
+    }
+}
