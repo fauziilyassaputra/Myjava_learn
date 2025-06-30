c48ea2e762d5322ceca6d0b03869ffdb68f6e4b1
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-06-30 10:54:45 +0700

    Create Constant.java

---
+++ java_OOP/33.StaticKeyword/data/Constant.java
@@ -0,0 +1,9 @@
+package hollow.special.operation.data;
+
+public class Constant {
+
+    // Static field
+    public static final String APPLICATION = "belajar java";
+    public  static int VERSION = 1;
+
+}
