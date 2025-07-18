6fb6e59789806c714e93f63ed0c43cf4c32d9730
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-07-18 17:40:54 +0700

    Create Lingkaran.java

---
+++ HitungLuasLingkaran/Lingkaran.java
@@ -0,0 +1,14 @@
+package dataStructure;
+
+public class Lingkaran {
+    double jari;
+
+    public Lingkaran(double jari){
+        this.jari = jari;
+    }
+
+    public double LuasLingkaran(){
+        return jari * 3.14 ;
+    }
+
+}
