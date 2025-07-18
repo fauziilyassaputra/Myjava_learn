a5d23cec0e9eda9128f2bcff685aa16191503a8c
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-07-18 17:40:34 +0700

    Create LuasLingkaranMain.java

---
+++ HitungLuasLingkaran/LuasLingkaranMain.java
@@ -0,0 +1,12 @@
+import dataStructure.Lingkaran;
+
+public class LuasLingkaranMain {
+    public static void main(String[] args) {
+
+        int jari = 15;
+
+        Lingkaran myLuasLIngkaran = new Lingkaran(jari);
+        System.out.println(myLuasLIngkaran.LuasLingkaran());
+
+    }
+}
